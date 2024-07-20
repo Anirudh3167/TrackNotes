7bf2a5071c5583d12905b94bd4ab7c931d8c3a6f
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2024-07-20 16:08:31 +0530

    problem resolved

---
+++ docs/Problems and Solutions/website.md
@@ -0,0 +1,11 @@
+<h1 align="center"> Problems Encountered with Solutions </h1>
+
+## The ( ...) Problem
+Error Sample : <br />
+```<span>User Authentication (OAuth, Email Verification, Dedicated pages,...)</span>``` <br />
+Error: <br />
+``` Error: `'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.  react/no-unescaped-entities ```
+Solution : <br />
+```<span>{`User Authentication (OAuth, Email Verification, Dedicated pages,...)`}</span>``` <br />
+
+<br />
--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
       <span> Markdown Support </span>
       <span> Allow to update Content Remotely </span>
       <span> Mobile App notes creation, Editing and Markdown based Display </span>
-      <span> User Authentication (OAuth, Email Verification, Dedicated pages,...) </span>
-      <span> Dashboard Section (With Stats of User usage, Notes,...) </span>
-      <span> Profile (User's Data, Notes, Files, ...) </span>
+      <span>{`User Authentication (OAuth, Email Verification, Dedicated pages,...)`}</span>
+      <span> {`Dashboard Section (With Stats of User usage, Notes,...)`} </span>
+      <span> {`Profile (User's Data, Notes, Files, ...)`} </span>
       <Link href="/my-works" className="items-center justify-center flex w-full text-center text-4xl">My Works</Link>
     </main>
   );
