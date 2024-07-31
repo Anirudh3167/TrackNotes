deee90da59b9431a8f51113e6a9969a403f4e739
Anirudh <77240291+Anirudh3167@users.noreply.github.com>
2024-08-01 01:09:01 +0530

    Basic Auth done + notes edit is not working + bug fix for MONGODB_URI

--- website/src/db/Users.ts
+++ website/src/db/Users.ts
@@ -5,7 +5,7 @@ import Users from "./Models/Users";
 export async function GetUser({ username, email, password } : any) {
   // Check based on unique username for now.
   console.log("GetUser: ");
-  await mongoose.connect(process.env.NEXT_PUBLIC_MONGOOSE_URI || '');
+  await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI || '');
   let d = await Users.find({ username, password });
   console.log(d);
   return d;
@@ -13,7 +13,7 @@ export async function GetUser({ username, email, password } : any) {
 
 export async function RegisterUser({ username, email, password } : any) {
   console.log("RegisterUser: ");
-  await mongoose.connect(process.env.NEXT_PUBLIC_MONGOOSE_URI || '');
+  await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI || '');
   
   const d = await Users.create({ username, email, password, Notes: [] });
   console.log(d);
@@ -21,14 +21,14 @@ export async function RegisterUser({ username, email, password } : any) {
 }
 
 export async function GetUserNotes({ username } : { username : string }) {
-  await mongoose.connect(process.env.NEXT_PUBLIC_MONGOOSE_URI || '');
+  await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI || '');
   let d = await Users.find({ username });
   if (!d || d.length === 0) return [];
   return d[0].Notes;
 }
 
 export async function UpdateUserNotes({ username, noteId, content } : { username: string, noteId : string, content : string }) {
-  await mongoose.connect(process.env.NEXT_PUBLIC_MONGOOSE_URI || '');
+  await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI || '');
   let d = null;
   if ((await GetUserNotes({ username })).filter((note: any) => note.noteId === noteId).length === 0)
     d = await Users.updateOne({ username }, { $push: { Notes: { noteId, access: 'private', content } } });
